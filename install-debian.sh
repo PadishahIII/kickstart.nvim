07e635c57aa443dbd3558aafd43f9560f7862f59
padishah <straystrayer@gmail.com>
2025-09-08 11:20:19 +0800

    update install script

--- install-debian.sh
+++ install-debian.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt-get install neovim
-sudo apt-get install ripgrep
-sudo apt-get install fd
-git clone https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts || exit
-./install.sh Hack
+sudo apt-get -y install neovim
+sudo apt-get -y install ripgrep
+sudo apt-get -y install fd
+# git clone https://github.com/ryanoasis/nerd-fonts.git
+# cd nerd-fonts || exit
+# ./install.sh Hack
+brew install --cask font-hack-nerd-font
