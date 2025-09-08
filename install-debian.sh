9183ed220ca4cc7dcea8a05dfe3e0a48fe01c708
padishah <straystrayer@gmail.com>
2025-09-08 11:49:45 +0800

    update install script

--- install-debian.sh
+++ install-debian.sh
@@ -2,7 +2,14 @@
 # Prerequisite:
 # - Homebrew installed
 
+brew update
+brew install node
 brew install neovim
 brew install ripgrep
 brew install fd
 brew install --cask font-hack-nerd-font
+
+# Python LSP
+# pip install pyright
+# pip install python-lsp-server[all]
+# pip install python-lsp-ruff # Fast linting
